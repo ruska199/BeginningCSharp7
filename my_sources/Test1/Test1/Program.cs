f35efd117b6e6d638a3ffc254bb457db85a47ca6
ruska199 <ruslanlozhkin@yahoo.com>
2018-12-25 22:13:37 +0200

    formating numeric strings with doubles and strings

--- my_sources/Test1/Test1/Program.cs
+++ my_sources/Test1/Test1/Program.cs
@@ -15,11 +15,21 @@ namespace Test1
         {
             int myInt = 550;
             int[] arr = {100, 200, 300, 400};
+            double myDouble = 13.543267;
+            string text = "Some text";
             Console.WriteLine($"The value: {myInt:C}");
             Console.WriteLine($"The value: {myInt:F}");
             Console.WriteLine("|{0,  10}|", myInt);
             Console.WriteLine("|{1,  5}{0, -5}|", arr[0], arr[1]);
             Console.WriteLine("|{2, -5}{1, 5}|", arr[0], arr[1], arr[2]);
+            Console.WriteLine("_______________________________________\n");
+            Console.WriteLine($"{myDouble, -10:F4}");
+            Console.WriteLine($"{myDouble, 10:F2}");
+            Console.WriteLine($"{myDouble, -10:E4}");
+            Console.WriteLine($"{myInt, -10:X}");
+            Console.WriteLine("_______________________________________\n");
+            Console.WriteLine($"{text, 20}");
+            Console.WriteLine($"{text, 15}");
             Console.ReadKey();
         }
     }
