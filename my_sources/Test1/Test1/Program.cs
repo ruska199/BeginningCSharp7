80c4233bf139a20b0a07165a1fc426c6d28e311c
ruska199 <ruslanlozhkin@yahoo.com>
2018-12-25 21:52:08 +0200

    Formatting Numeric String     ("{0, 10}", variable)

---
+++ my_sources/Test1/Test1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Program
+    {   
+        
+        //Some example of formatting numeric strings
+
+        static void Main(string[] args)
+        {
+            int myInt = 550;
+            int[] arr = {100, 200, 300, 400};
+            Console.WriteLine($"The value: {myInt:C}");
+            Console.WriteLine($"The value: {myInt:F}");
+            Console.WriteLine("|{0,  10}|", myInt);
+            Console.WriteLine("|{1,  5}{0, -5}|", arr[0], arr[1]);
+            Console.WriteLine("|{2, -5}{1, 5}|", arr[0], arr[1], arr[2]);
+            Console.ReadKey();
+        }
+    }
+}
